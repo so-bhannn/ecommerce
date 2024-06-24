9696e09960d76474b6331628106e59b21c02960b
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2024-06-24 15:08:33 +0530

    'ecommerce'

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout, name='logout'),
+    path('postsignup/', views.postsignup),
+    path('postlogin/', views.postlogin),
 ] 
