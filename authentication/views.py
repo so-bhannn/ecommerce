a9605ac21202a2c45fb51f028868f564d84a05ee
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2024-06-24 15:08:33 +0530

    'ecommerce'

--- authentication/views.py
+++ authentication/views.py
@@ -1,32 +1,82 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate,login
+import pyrebase
+import os
+from dotenv import load_dotenv
+
 # Create your views here.
 
+load_dotenv()
+
+config={
+    "apiKey":os.getenv("FIREBASE_API_KEY"),
+    "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+    "databaseURL":os.getenv("FIREBASE_DATABASE_URL"),
+    "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+    "messagingSenderId":os.getenv("FIREBASE_MESSAGING_SENDER_ID") ,
+    "appId": os.getenv("FIREBASE_APP_ID"),
+}
+
+firebase=pyrebase.initialize_app(config)
+auth=firebase.auth()
+db=firebase.database()
+
+def home(request):
+    return render(request,'index.html')
+
 def signup(request):
+    return render(request,'authentication/signup.html')
+
+def login(request):  
+    return render(request,'authentication/login.html')
+
+def logout(request):
+    try:
+        del request.session['idToken']
+    except:
+        pass
+    return render(request,'services/index.html')
+
+def postsignup(request):
     if request.method=='POST':
-        username=username.POST['username']
-        fname=fname.POST['fname']
-        lname=lname.POST['lname']
-        email=email.POST['email']
-        pass1=pass1.POST['pass1']
-        pass2=pass2.POST['pass2']
+        username=request.POST.get('username')
+        fname=request.POST.get('fname')
+        lname=request.POST.get('lname')
+        email=request.POST.get('email')
+        pass1=request.POST.get('pass1')
+        pass2=request.POST.get('pass2')
         
-        myuser=User.object.create_user(username,email,pass1)
-        myuser.first_name=fname
-        myuser.last_name=lname
+        if pass1!=pass2:
+            messages.error(request, "Passwords Didn't match.")
+            return redirect('signup')
 
-        myuser.save()
+        if len(username)>20:
+            messages.error(request, "Username too long.")
+            return redirect('signup')
 
-        messages.success(request,"Your Account has been successfully created.")
+        # if not username.isalnum():
+        #     messages.error(request, "Username should contain letters, numbers and special characters.")
+        #     return redirect('signup')
+        try:
+            user=auth.create_user_with_email_and_password(email,pass1)
+            idToken=request.session[user['localId']]
+        except:
+            message="Account cannot be made now, try again after some time."
+            return render(request,'authentication/signup.html',{"alert":message})
+        return render(request,'authentication/login.html',{'msg':message})
+    return render(request,'authentication/signup.html')
 
-        return redirect('login')
-
-
-
-def login(request):
-    loggedin=request.GET.get("loggedin","off")
-
-
-    
-    return render(request, 'authentication/login.html')
+def postlogin(request):
+    if request.method=='POST':
+        email=request.POST.get('email')
+        pass1=request.POST.get('passw')
+        try:
+            user=auth.sign_in_with_email_and_password(email,pass1)
+            request.session['uid']=str(user['idToken'])
+        except:
+            message="Invalid Credentials"
+            return render(request,'authentication/login.html',{'alert':message})    
+    return render(request, 'services/index.html',{"email":email})
