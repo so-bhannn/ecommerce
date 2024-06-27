625aebe85987496b602a21b81b7e216e3ec4d596
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2024-06-28 00:01:40 +0530

    update

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from services import views
-from authentication import views
 from django.conf import settings
 from django.conf.urls.static import static
 
